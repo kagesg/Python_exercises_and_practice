ee9e9f1b07053f359dc3df5ac29a8396b6a02566
Gustavo Kagesawa <gustavokagesawa@gmail.com>
2022-01-28 15:29:17 -0300

    Repository creation
    Repository creation and 2 exercises solution

---
+++ Python_basic_1/ex2.py
@@ -0,0 +1,6 @@
+'''
+2. Write a Python program to get the Python version you are using.
+'''
+import sys
+
+print("In this system, the version of Python installed is:\n{}\n\tOr, more detailed info:\n\t{}".format(sys.version,sys.version_info))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python_exercises_and_practice
  Repository to save my python exercises
+ Exercises from: https://www.w3resource.com/python-exercises/
